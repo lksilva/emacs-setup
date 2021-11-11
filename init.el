ecbdc2573a5016ce29a27d9050978cb867d8339e
Lucas Souza <lucas_s.s@hotmail.com>
2021-11-11 14:30:02 -0300

    Commited relevate setup

---
+++ init.el
@@ -0,0 +1,123 @@
+;;; init.el --- Spacemacs Initialization File -*- no-byte-compile: t -*-
+;;
+;; Copyright (c) 2012-2021 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;; Without this comment emacs25 adds (package-initialize) here
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+;; Avoid garbage collection during startup.
+;; see `SPC h . dotspacemacs-gc-cons' for more info
+(defconst emacs-start-time (current-time))
+(setq gc-cons-threshold 402653184 gc-cons-percentage 0.6)
+(load (concat (file-name-directory load-file-name)
+              "core/core-versions.el")
+      nil (not init-file-debug))
+(load (concat (file-name-directory load-file-name)
+              "core/core-load-paths.el")
+      nil (not init-file-debug))
+(load (concat spacemacs-core-directory "core-dumper.el")
+      nil (not init-file-debug))
+
+;; Remove compiled core files if they become stale or Emacs version has changed.
+(load (concat spacemacs-core-directory "core-compilation.el")
+      nil (not init-file-debug))
+(load spacemacs--last-emacs-version-file t (not init-file-debug))
+(when (or (not (string= spacemacs--last-emacs-version emacs-version))
+          (spacemacs//dir-contains-stale-byte-compiled-files-p
+           spacemacs-core-directory))
+  (spacemacs//remove-byte-compiled-files-in-dir spacemacs-core-directory))
+;; Update saved Emacs version.
+(unless (string= spacemacs--last-emacs-version emacs-version)
+  (spacemacs//update-last-emacs-version))
+
+(if (not (version<= spacemacs-emacs-min-version emacs-version))
+    (error (concat "Your version of Emacs (%s) is too old. "
+                   "Spacemacs requires Emacs version %s or above.")
+           emacs-version spacemacs-emacs-min-version)
+  ;; Disable file-name-handlers for a speed boost during init
+  (let ((file-name-handler-alist nil))
+    (require 'core-spacemacs)
+    (spacemacs/dump-restore-load-path)
+    (configuration-layer/load-lock-file)
+    (spacemacs/init)
+    (configuration-layer/stable-elpa-init)
+    (configuration-layer/load)
+    (spacemacs-buffer/display-startup-note)
+    (spacemacs/setup-startup-hook)
+    (spacemacs/dump-eval-delayed-functions)
+    (when (and dotspacemacs-enable-server (not (spacemacs-is-dumping-p)))
+      (require 'server)
+      (when dotspacemacs-server-socket-dir
+        (setq server-socket-dir dotspacemacs-server-socket-dir))
+      (unless (server-running-p)
+        (message "Starting a server...")
+        (server-start)))))
+
+;; Custom setup
+(setq package-selected-packages '(clojure-mode lsp-mode cider lsp-treemacs flycheck company))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+;;(add-hook 'clojure-mode-hook 'lsp)
+;;(add-hook 'clojurescript-mode-hook 'lsp)
+;;(add-hook 'clojurec-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-minimum-prefix-length 1
+      lsp-lens-enable t
+      lsp-signature-auto-activate nil
+                                        ; lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
+                                        ; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
+      )
+
+;; Overring keyboard
+
+;; Use SHIFT+arrow to move between the windows
+(windmove-default-keybindings)
+
+;; Autoload
+;; Paredit
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'clojurescript-mode-hook #'paredit-mode)
+(add-hook 'clojurec-mode-hook #'paredit-mode)
+
+(global-display-line-numbers-mode)
+
+(global-set-key (kbd "M-s-p") 'projectile-switch-open-project)
+(global-set-key (kbd "M-s-f") 'helm-find-files)
+(global-set-key (kbd "M-s-c") 'cider-repl-clear-buffer)
+(global-set-key (kbd "s-L") 'cider-eval-file)
+(global-set-key (kbd "s-N") 'cider-repl-set-ns)
+(global-set-key (kbd "C-c g") 'magit)
